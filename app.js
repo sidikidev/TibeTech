51150d97ccac8c5a693d01091a2d60a2101a02fe
sidikidev <116438756+sidikidev@users.noreply.github.com>
2025-11-08 23:54:30 +0000

    Fix MySQL connection for Railway2

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true // obligatoire pour Railway MySQL
+  }
 });
 
 app.get("/", async function (req, res) {
